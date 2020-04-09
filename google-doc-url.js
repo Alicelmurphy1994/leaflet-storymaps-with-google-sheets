5b57dc76f44b5a18716c0d5ffd378a748f4b66d7
Alicelmurphy1994 <63109682+Alicelmurphy1994@users.noreply.github.com>
2020-04-09 19:24:30 +0100

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1AO6XHL_0JafWZF4KEejkdDNqfuZWUk3SlNlQ6MjlRFM/';
+https://docs.google.com/spreadsheets/d/1w2lK2VoWKW2S5jsSHC41BugZFX_uu3d6iPvURwLj-u4/edit#gid=0
